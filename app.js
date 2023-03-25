7e378f705fa62c91115c967829c41aded5e50706
Arturo Simon <artysimon@gmail.com>
2023-03-25 11:59:28 -0400

    Handle unhandled routes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 
 const connectDatabase = require('./config/database');
 const errorMiddleware = require('./middlewares/errors');
-
+const ErrorHandler = require('./utils/errorHandler');
 
 // Setting up config.ev file variables
 dotenv.config({ path: './config/config.env' });
@@ -28,6 +28,12 @@ const jobs = require('./routes/jobs');
 
 
 app.use('/api/v1', jobs);
+
+// Handle unhandled routes,
+// NOTE: must be placed after all routes
+app.all('*', (req, res, next) => {
+    next(new ErrorHandler(`${req.originalUrl} route not found`, 404));
+});
 
 // Middleware to handle errors
 app.use(errorMiddleware);
